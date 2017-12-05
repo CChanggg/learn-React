f7c8ac4865e2d758f8f1e44b7186eb3d5be651eb
CChanggg <373893135@qq.com>
2017-12-05 16:11:54 +0800

    评论功能（二）

--- comment-app/src/CommentInput.js
+++ comment-app/src/CommentInput.js
@@ -1,12 +1,39 @@
-import React, { Component } from 'react'
+import React, { Component, PropTypes } from 'react'
 
 class CommentInput extends Component {
+  static propTypes = {
+    onSubmit: PropTypes.func
+  }
+
   constructor () {
     super()
     this.state = {
       username: '',
       content: ''
     }
+  }
+
+  componentWillMount () {
+    this._loadUsername()
+  }
+
+  componentDidMount () {
+    this.textarea.focus()
+  }
+
+  _loadUsername () {
+    const username = localStorage.getItem('username')
+    if (username) {
+      this.setState({ username })
+    }
+  }
+
+  _saveUsername (username) {
+    localStorage.setItem('username', username)
+  }
+
+  handleUsernameBlur (event) {
+    this._saveUsername(event.target.value)
   }
 
   handleUsernameChange (event) {
@@ -26,6 +53,7 @@ class CommentInput extends Component {
       this.props.onSubmit({
         username: this.state.username,
         content: this.state.content,
+        createdTime: +new Date()
       })
     }
     this.setState({ content: '' })
@@ -39,6 +67,7 @@ class CommentInput extends Component {
           <div className='comment-field-input'>
             <input
               value={this.state.username}
+              onBlur={this.handleUsernameBlur.bind(this)}
               onChange={this.handleUsernameChange.bind(this)} />
           </div>
         </div>
@@ -46,6 +75,7 @@ class CommentInput extends Component {
           <span className='comment-field-name'>评论内容：</span>
           <div className='comment-field-input'>
             <textarea
+              ref={(textarea) => this.textarea = textarea}
               value={this.state.content}
               onChange={this.handleContentChange.bind(this)} />
           </div>
